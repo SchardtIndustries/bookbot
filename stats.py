1549dcb78b01a7d91dfc5b4e1cc143bb2292c0ec
M.Schardt <matthew.schardt45@gmail.com>
2025-07-27 09:16:36 -0400

    added character counter

--- stats.py
+++ stats.py
@@ -2,3 +2,9 @@ def count(text):
     word_count = len(text.split())
     return word_count
 
+def count_characters(text_string):
+    char_counts = {}
+    for char in text_string:
+        lower_char = char.lower()
+        char_counts[lower_char] = char_counts.get(lower_char, 0) + 1
+    return char_counts
