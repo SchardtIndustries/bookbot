ffc373177d3e3cccf7948087208b96d1d012c946
M.Schardt <matthew.schardt45@gmail.com>
2025-07-27 11:32:58 -0400

    formatted report

--- stats.py
+++ stats.py
@@ -6,5 +6,15 @@ def count_characters(text_string):
     char_counts = {}
     for char in text_string:
         lower_char = char.lower()
-        char_counts[lower_char] = char_counts.get(lower_char, 0) + 1
+        if lower_char.isalpha():
+            char_counts[lower_char] = char_counts.get(lower_char, 0) + 1
     return char_counts
+
+def sort_character_counts(char_counts_dict):
+    sorted_list = []
+    for char, count in char_counts_dict.items():
+        sorted_list.append({"char": char, "num": count})
+
+    sorted_list.sort(reverse=True, key=lambda x: (x["num"]))
+    return sorted_list
+
