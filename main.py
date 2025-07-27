d325693299236a3893cbdac2da2713c53279741e
M.Schardt <matthewschardt@schardtindustries.com>
2025-07-27 09:16:36 -0400

    added character counter

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from stats import count
+from stats import count_characters
 def get_book_text(filepath):
      with open (filepath, 'r') as file:
         content = file.read()
         return content
+
 def main():
     t = get_book_text("books/frankenstein.txt")
-    from stats import count
-    count(t)
     word_count = count(t)
+    cc = count_characters(t)
     print (f"{word_count} words found in the document")
-
+    print (cc)
 main()
