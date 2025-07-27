9458b4f9e645fc0ee659c25d1c8384993fc09ad1
M.Schardt <matthewschardt@schardtindustries.com>
2025-07-27 11:32:58 -0400

    formatted report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import count
 from stats import count_characters
+from stats import sort_character_counts
+
 def get_book_text(filepath):
      with open (filepath, 'r') as file:
         content = file.read()
@@ -9,6 +11,16 @@ def main():
     t = get_book_text("books/frankenstein.txt")
     word_count = count(t)
     cc = count_characters(t)
-    print (f"{word_count} words found in the document")
-    print (cc)
+    scc = sort_character_counts(cc)
+    print ("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in scc:
+        item["char"]
+        item['num']
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
 main()
