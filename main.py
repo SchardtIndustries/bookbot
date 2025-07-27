768b8478abfc4806155e550b7506191544b45f35
M.Schardt <matthewschardt@schardtindustries.com>
2025-07-27 12:03:45 -0400

    added variable usage

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from stats import count
 from stats import count_characters
 from stats import sort_character_counts
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
-     with open (filepath, 'r') as file:
+    with open (filepath, 'r') as file:
         content = file.read()
         return content
 
 def main():
-    t = get_book_text("books/frankenstein.txt")
+    t = get_book_text(sys.argv[1])
     word_count = count(t)
     cc = count_characters(t)
     scc = sort_character_counts(cc)
     print ("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -23,4 +28,5 @@ def main():
         print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
 
-main()
+if __name__ == "__main__":
+    main()
